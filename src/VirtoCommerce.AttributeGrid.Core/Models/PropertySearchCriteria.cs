bbcae449bb59606c489bf5e50f83cf87a766fffd
loid345 <volanar@tut.by>
2026-01-23 14:55:10 +0300

    Fix property search filtering and core dependency

---
+++ src/VirtoCommerce.AttributeGrid.Core/Models/PropertySearchCriteria.cs
@@ -0,0 +1,14 @@
+using VirtoCommerce.Platform.Core.Common;
+
+namespace VirtoCommerce.AttributeGrid.Core.Models;
+
+public class PropertySearchCriteria : SearchCriteriaBase
+{
+    public string Keyword { get; set; }
+    public string CatalogId { get; set; }
+    public string CategoryId { get; set; }
+    public string ValueType { get; set; }
+    public string PropertyType { get; set; }
+    public bool? IsFilterable { get; set; }
+    public bool? IsDictionary { get; set; }
+}
